05ebeba3a196798a250242857fb311e9c3cc1ad3
Beni Regez <benjamin.regez@gmx.ch>
2026-04-03 15:32:58 +0200

    Add helper python code file for 06.2.

---
+++ ex06/assets/ex06_02.py
@@ -0,0 +1,79 @@
+import copy
+
+class chessboard:
+    def __init__(self, queens: list[int]):
+        self.queens = queens
+        self.size = len(queens)
+        self.h = self.get_heuristic(self.queens)
+
+    def print_board(self) -> None:
+        for j in range(self.size):
+            for i in range(self.size):
+                if self.queens[i] == j:
+                    print("[Q]", end="")
+                else:
+                    print("[ ]", end="")
+            print()
+    
+    def get_heuristic(self, queens) -> int:
+        h = 0
+        for i in range(self.size):
+            for j in range(i+1, self.size):
+                # Check same row
+                if queens[i] == queens[j]:
+                    h += 1
+                # Check same diagonal
+                if abs(queens[i] - queens[j]) == abs(i-j):
+                    h += 1
+        return h
+
+    def get_h_board(self) -> list[list[int]]:
+        h_board = [[0 for _ in range(self.size)] for _ in range(self.size)]
+
+        for col in range(self.size):
+            h_board[col][self.queens[col]] = -1
+        
+        for col in range(self.size):
+            col_queen = self.queens[col]
+            for row in range(self.size):
+                copy_queens = copy.deepcopy(self.queens)        
+                if h_board[col][row] != -1:
+                    copy_queens = copy.deepcopy(self.queens)
+                    copy_queens[col] = row
+                    h_board[col][row] = self.get_heuristic(copy_queens)
+
+        return h_board
+
+    def print_h_board(self) -> None:
+        h_board = self.get_h_board()
+        rows = ["" for _ in range(self.size)]
+
+        for col_idx, col in enumerate(h_board):
+            for row_idx, row in enumerate(col):
+                if row == -1:
+                    # print("[Q]", end="")
+                    rows[row_idx] += "[Q]"
+                else:
+                    if row > self.h:
+                        # print(f"\033[31m[{row}]\033[0m", end="")
+                        rows[row_idx] += f"\033[31m[{row}]\033[0m"
+                    elif row < self.h:
+                        # print(f"\033[32m[{row}]\033[0m", end="")
+                        rows[row_idx] += f"\033[32m[{row}]\033[0m"
+                    else:
+                        # print(f"\033[33m[{row}]\033[0m", end="")
+                        rows[row_idx] += f"\033[33m[{row}]\033[0m"
+
+        for row in rows:
+            print(row)
+
+if __name__ == "__main__":
+    # Initial candidate
+    board = chessboard([2, 3, 1, 2, 4])
+    board.print_h_board()
+    print()
+
+    # Iteration 1
+    board = chessboard([0, 3, 1, 2, 4])
+    board.print_h_board()   # Iteration 1
+    print()
